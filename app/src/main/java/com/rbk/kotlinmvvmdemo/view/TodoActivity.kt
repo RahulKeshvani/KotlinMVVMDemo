ebe9995a025ee68752ed4169bfc4a08119345b57
Rahul Keshvani <rahulkeshvani@gmail.com>
2021-03-07 01:06:00 +0530

    room dependency

---
+++ app/src/main/java/com/rbk/kotlinmvvmdemo/view/TodoActivity.kt
@@ -0,0 +1,12 @@
+package com.rbk.kotlinmvvmdemo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
---
+++ app/src/main/java/com/rbk/kotlinmvvmdemo/view/TypeSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.rbk.kotlinmvvmdemo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TypeSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_type_selection)
+    }
+}
