c2b3471a612049330a559bfd9ffee060fd2a7634
Rahul Keshvani <rahulkeshvani@gmail.com>
2021-03-10 01:57:32 +0530

    room setup

---
+++ app/src/main/java/com/rbk/kotlinmvvmdemo/model/AppDatabase.kt
@@ -0,0 +1,2 @@
+package com.rbk.kotlinmvvmdemo.model
+
---
+++ app/src/main/java/com/rbk/kotlinmvvmdemo/view/AddOrEditTodoActivity.kt
@@ -0,0 +1,12 @@
+package com.rbk.kotlinmvvmdemo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddOrEditTodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_or_edit_todo)
+    }
+}
